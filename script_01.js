c61542d3b82beb33a77e6685f1de21aae3f9029a
Clapfix <ronny.perner@kabelmail.de>
2022-01-17 10:37:56 +0100

    03a Konstanten

--- script_01.js
+++ script_01.js
@@ -16,10 +16,32 @@
 
 /** 02 Deklaration + Wertzuweisung I*/
 
-let firstName; // Deklaration (Definition)
-firstName = "Max"; // Wertzuweisung (Assigment)
-console.log(firstName); // Ausgabe
+// let firstName; // Deklaration (Definition)
+// firstName = "Max"; // Wertzuweisung (Assigment)
+// console.log(firstName); // Ausgabe
 
-let familyName = "Mütze";
-console.log(familyName);
-console.log(firstName+" "+familyName);
+// let familyName = "Mütze";
+// console.log(familyName);
+// console.log(firstName+" "+familyName);
+
+/** 03 Deklaration + Wertzuweisung II*/
+
+// let firstName,familyName;
+// firstName = prompt("Bitte Vornamen eingeben:");
+// familyName = prompt("Bitte Nachnamen eingeben:");
+// console.log(firstName+" "+familyName);
+
+// JS ist eine untypisierte Sprache! | untyped
+// let test;
+// test = "Hi";
+// test = 2;
+// test = true;
+
+// console.log("Datentyp: " + typeof test);
+// console.log("Inhalt: " +test);
+
+/**03a Konstanten */
+
+const test = "hi"; //Variable mit KONSTANTEM Inhalt
+test = 2; // KEINE neue Zuweisung zur LZ möglich!
+console.log("Inhalt: " +test);
